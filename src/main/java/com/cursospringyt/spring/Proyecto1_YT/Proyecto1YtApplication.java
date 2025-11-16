381abdc2244d2ea255d0283a2b4d78b404f05af6
Carlos <carlosbuttec@gmail.com>
2025-11-16 00:55:46 -0600

    creacion del proyecto

---
+++ src/main/java/com/cursospringyt/spring/Proyecto1_YT/Proyecto1YtApplication.java
@@ -0,0 +1,15 @@
+package com.cursospringyt.spring.Proyecto1_YT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Proyecto1YtApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(Proyecto1YtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospringyt/spring/Proyecto1_YT/Proyecto1YtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospringyt.spring.Proyecto1_YT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1YtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
